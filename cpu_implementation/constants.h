58abb51e3f3bb2d8bd1d0b8a39a78bd02809b8b5
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-04-26 18:06:16 +0200

    ref #1: Fix warnings on statics
    Fixed warnings on statics within the constants, apperantly GCC wants
    them before the const.

--- cpu_implementation/constants.h
+++ cpu_implementation/constants.h
@@ -1,16 +1,16 @@
 #pragma once
-const static int window_width = 640;
-const static int window_height = 640;
-const static int cell_width = 10;
-const static int cell_height = 10;
-const static int32_t max_fuel = 10;
-const static float max_initial_temprature = 200.f;
+static const int window_width = 640;
+static const int window_height = 640;
+static const int cell_width = 10;
+static const int cell_height = 10;
+static const int32_t max_fuel = 10;
+static const float max_initial_temprature = 200.f;
 
-const static float temprature_spread_up = 0.05f;
-const static float temprature_spread_diagonal_up = 0.025f;
-const static float temprature_spread_horizontal = 0.02f;
-const static float temprature_spread_diagonal_down = 0.01f;
-const static float temprature_spread_down = 0.01f;
+static const float temprature_spread_up = 0.05f;
+static const float temprature_spread_diagonal_up = 0.025f;
+static const float temprature_spread_horizontal = 0.02f;
+static const float temprature_spread_diagonal_down = 0.01f;
+static const float temprature_spread_down = 0.01f;
 
-const static float burning_threshold = 100.f;
-const static float fuel_consumption_heat_increase = 100.f;
+static const float burning_threshold = 100.f;
+static const float fuel_consumption_heat_increase = 100.f;
